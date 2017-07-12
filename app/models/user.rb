a96d6c4c5f72cc921bf1a55ab7c6bb46c7b48259
Sami Hamdan <samihamdan00@gmail.com>
2017-07-12 04:02:14 +0000

    merge fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,8 @@ class User < ActiveRecord::Base
   after_initialize {self.role ||=  :standard}
   
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
-=======
          :recoverable, :rememberable, :trackable, :validatable
          
   enum role: [:standard, :premium, :admin]
->>>>>>> user-story-5
+
 end
