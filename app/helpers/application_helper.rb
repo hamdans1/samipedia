0aaaef54d75ae20419a57de19dc58d9cede11e87
Sami Hamdan <samihamdan00@gmail.com>
2017-07-18 05:02:17 +0000

    user story 9 markdown syntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    
+    def markdown(text)
+        options = {
+            filter_html: true,
+            hard_wrap: true,
+            fenced_code_blocks: true
+        }
+        
+        extensions = {
+            autolink: true
+        }
+        
+        renderer = Redcarpet::Render::HTML.new(options)
+        markdown = Redcarpet::Markdown.new(renderer, extensions)
+        
+        markdown.render(text.html_safe)
+    end
+    
 end
